f8a595570e9e8d822b4b838e045409dc139716e3
nancywong <nancypwong@gmail.com>
2015-03-07 18:50:54 -0500

    created search form and post request for input text

--- app/views.py
+++ app/views.py
@@ -1,8 +1,16 @@
 from app import app
+from flask import request, render_template
 
-@app.route('/')
+@app.route('/', methods=['GET','POST'])
 def search():
-    return "Search for nonprofit"
+    print "HI ARMAN"
+    if request.method == 'POST':
+        print "are we here at all"
+        d = request.form.getlist('search')
+        t = d[0]
+        print t
+
+    return render_template('index.html')
 
 @app.route('/results')
 def results():
