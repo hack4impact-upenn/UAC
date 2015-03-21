6dfd4b56026d0fd361b8a53141c76fbd23a0ad02
nancywong <nancypwong@gmail.com>
2015-03-21 12:36:29 -0400

    for search query, print results (name, ein, city, state, tax period) to terminal

--- app/views.py
+++ app/views.py
@@ -1,14 +1,35 @@
 from app import app
 from flask import request, render_template
-import requests
+from attrdict import AttrDict
+from urllib import quote_plus
+import requests, json
+
+RESULTS_PER_PAGE = 25
 
 @app.route('/', methods=['GET','POST'])
 def search():
     if request.method == 'POST':
         search_value = request.form.getlist('search')[0]
+        search_value = quote_plus(search_value)
         print search_value
 
-    print requests.get('https://projects.propublica.org/nonprofits/api/v1/search.json?q=propublica').content
+        query = 'https://projects.propublica.org/nonprofits/api/v1/search.json?q='
+
+        # TODO error checking
+        result = requests.get(query + search_value).content
+        result = json.loads(result) # convert to json obj
+
+        filings = result['filings']
+        num_results = result['total_results']
+
+        for i in range(0, min(RESULTS_PER_PAGE,num_results-1)):
+          org = filings[i]
+          print org['organization']['name']
+          print org['ein']
+          print org['organization']['city']
+          print org['organization']['state']
+          print org['organization']['tax_period']
+          print ""
 
     return render_template('index.html')
 
