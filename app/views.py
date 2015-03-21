03f0f9b9d02221762b407b9edab365bafa47b792
nancywong <nancypwong@gmail.com>
2015-03-21 15:41:31 -0400

    implemented working is_EIN

--- app/views.py
+++ app/views.py
@@ -2,7 +2,7 @@ from app import app
 from flask import request, render_template
 from attrdict import AttrDict
 from urllib import quote_plus
-import requests, json
+import requests, json, re
 
 RESULTS_PER_PAGE = 25
 
@@ -16,6 +16,7 @@ def search():
         result = query(search_value)
 
         if is_EIN(search_value):
+          # ein = parse_EIN(search_value)
           org = result['organization']
           print org['name']
           print org['ein']
@@ -24,9 +25,11 @@ def search():
           print org['tax_period']
 
         else:
+          print "HERE I AM 2"
           filings = result['filings']
           num_results = result['total_results']
 
+          print "HERE I AM 3"
           for i in range(0, min(RESULTS_PER_PAGE,num_results)-1):
             org = filings[i]['organization']
             print org['name']
@@ -43,13 +46,13 @@ def search():
 def query(search_value):
   # use pattern matching to check if search value is EIN or org name
   if is_EIN(search_value):
+    #print "is EIN"
     query = 'https://projects.propublica.org/nonprofits/api/v1/organizations/'
 
     result = requests.get(query + search_value + '.json').content
 
   else:
     query = 'https://projects.propublica.org/nonprofits/api/v1/search.json?q='
-
     # TODO error checking
     result = requests.get(query + search_value).content
     
@@ -57,6 +60,20 @@ def query(search_value):
   return result
 
 def is_EIN(search_value):
+  # TODO double check error logic
+  # 1. strip non-alphanumeric, check is number and length <= 9
+  # remove all non-alphanumeric characters
+  check_val = re.sub(r'[^a-zA-Z0-9]','', search_value)
+  if (not check_val.isdigit()) or (len(check_val) > 9):
+    return False
+
+  # pad with leading 0's if len < 9
+  if len(check_val) < 9:
+    check_val = check_val.zfill(9)
+
+  # 2. check if ein is valid using API request
+
+
   return True
 
 @app.route('/results')
