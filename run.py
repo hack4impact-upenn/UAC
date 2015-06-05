a060b6ab32bc552549da48021f6cbd02cb3333f5
Arman Tokanov <armatoka@gmail.com>
2015-06-05 00:40:51 -0700

    edited app.py with production settings again x6

--- run.py
+++ run.py
@@ -1,4 +1,6 @@
 #!ENV/bin/python
+import os
 from app import app
 # for developing use app.run(debug=True)
-app.run(host='0.0.0.0', port=0)
+port = int(os.environ.get('PORT', 5000))
+app.run(host='0.0.0.0', port=port)
